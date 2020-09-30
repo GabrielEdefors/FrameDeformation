943839da4caf8761e008300225e6b79219f0ffd0
Gabriel Edefors <gabriel_edefors@hotmail.com>
2020-09-30 20:44:09 +0200

    Added a solver class and methods for computing sectional forces in the beam

--- FrameDeformation/Beam.cs
+++ FrameDeformation/Beam.cs
@@ -9,7 +9,7 @@ using Rhino.Geometry;
 
 namespace FrameDeformation
 {
-	class Bar
+	class Beam
 	{
 		// Properties	
 		public List<Node> Nodes = new List<Node>();
@@ -17,27 +17,27 @@ namespace FrameDeformation
 		public double StiffnessModulus { get; set; } = 0.0;
 		public double ElemLength { get; set; } = 0.0;
 		public double MomentArea { get; set; } = 0.0;
+		public double NrEvalPoints { get; set; } = 100;
+		public double TranverseLoad { get; set; } = 0;
 
 		private LinearAlgebra.Matrix<double> G = LinearAlgebra.Matrix<double>.Build.Dense(6, 6);
 
 		// Constructor
-		public Bar(Node node1,
+		public Beam(Node node1,
 				   Node node2,
 				   double area,
 				   double stiffnessModulus,
-				   double momentArea)
+				   double momentArea,
+				   double transverseLoad)
 		{
 			Nodes.Add(node1);
 			Nodes.Add(node2);
 			StiffnessModulus = stiffnessModulus;
 			Area = area;
 			MomentArea = momentArea;
-		}
+			TranverseLoad = transverseLoad;
 
-		public LinearAlgebra.Matrix<double> ComputeStiffnessMatrix()
-		{
-			LinearAlgebra.Matrix<double> K = LinearAlgebra.Matrix<double>.Build.Dense(6, 6);
-
+			// Compute the element lenght and the transformation matrix G
 			double x1 = Nodes[0].Point.X;
 			double y1 = Nodes[0].Point.Y;
 
@@ -45,6 +45,26 @@ namespace FrameDeformation
 			double y2 = Nodes[1].Point.Y;
 
 			ElemLength = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+			double nxx = (x2 - x1) / ElemLength;
+			double nyx = (y2 - y1) / ElemLength;
+
+			G[0, 0] = nxx;
+			G[0, 1] = nyx;
+			G[1, 0] = -nyx;
+			G[1, 1] = nxx;
+			G[2, 2] = 1;
+
+			G[3, 3] = nxx;
+			G[3, 4] = nyx;
+			G[4, 3] = -nyx;
+			G[4, 4] = nxx;
+			G[5, 5] = 1;
+		}
+
+		public LinearAlgebra.Matrix<double> ComputeStiffnessMatrix()
+		{
+			LinearAlgebra.Matrix<double> K = LinearAlgebra.Matrix<double>.Build.Dense(6, 6);
 
 			K[0, 0] = Area * StiffnessModulus / ElemLength;
 			K[0, 3] = -Area * StiffnessModulus / ElemLength;
@@ -72,26 +92,75 @@ namespace FrameDeformation
 			K[2, 5] = 4 * StiffnessModulus * MomentArea / ElemLength;
 
 			// Tranform to global element stiffness matrix using transformation matrix G
-
-			double nxx = (x2 - x1) / ElemLength;
-			double nyx = (y2 - y1) / ElemLength;
-
-			G[0, 0] = nxx;
-			G[0, 1] = nyx;
-			G[1, 0] = -nyx;
-			G[1, 1] = nxx;
-			G[2, 2] = 1;
-
-			G[3, 3] = nxx;
-			G[3, 4] = nyx;
-			G[4, 3] = -nyx;
-			G[4, 4] = nxx;
-			G[5, 5] = 1;
-
 			LinearAlgebra.Matrix<double> GT = G.Transpose();
 			LinearAlgebra.Matrix<double> KGlobal = (GT.Multiply(K)).Multiply(G);
 
 			return KGlobal;
+		}
+
+		public List<double> computeShearForce(List<double> globalElemDisp)
+		{
+			List<double> V = new List<double> { };
+
+			for(int i = 0; i < NrEvalPoints; i++)
+			{
+				double xi = i / NrEvalPoints * ElemLength;
+				LinearAlgebra.Vector<double> dBidx = LinearAlgebra.Vector<double>.Build.Dense(4);
+				dBidx[0] = 12 / Math.Pow(ElemLength, 3);
+				dBidx[1] = 6 / Math.Pow(ElemLength, 2);
+				dBidx[2] = - 12 / Math.Pow(ElemLength, 4);
+				dBidx[3] = 6 / Math.Pow(ElemLength, 2);
+
+				// Transform from global to local coordinates
+				LinearAlgebra.Vector<double> globalElemDispVector = LinearAlgebra.Vector<double>.Build.Dense(globalElemDisp.ToArray());
+				LinearAlgebra.Vector<double> localElemDisp = G.Multiply(globalElemDispVector);
+
+				// Extract displacements associated to beam dofs
+				LinearAlgebra.Vector<double> localElemDispBeam = LinearAlgebra.Vector<double>.Build.Dense(4);
+				localElemDispBeam[0] = localElemDisp[1];
+				localElemDispBeam[1] = localElemDisp[2];
+				localElemDispBeam[2] = localElemDisp[4];
+				localElemDispBeam[3] = localElemDisp[5];
+
+				// Particular solution for uniformly distributed load
+				double Vpi = -TranverseLoad * (xi - ElemLength / 2);
+
+				V.Add(-StiffnessModulus * MomentArea * (dBidx.DotProduct(localElemDispBeam) + Vpi));
+
+				return V;
+
+			}
+		}
+
+		public List<double> computebendingMoment(List<double> globalElemDisp)
+		{
+			List<double> M = new List<double> { };
+
+			for (int i = 0; i < NrEvalPoints; i++)
+			{
+				double xi = i / NrEvalPoints * ElemLength;
+				LinearAlgebra.Vector<double> Bi = LinearAlgebra.Vector<double>.Build.Dense(4);
+				Bi[0] = -6 / Math.Pow(ElemLength, 2) + 12 * xi / Math.Pow(ElemLength, 3);
+				Bi[1] = -4 / ElemLength + 6 * xi / Math.Pow(ElemLength, 2);
+				Bi[2] = 6 / Math.Pow(ElemLength, 2) - 12 * xi / Math.Pow(ElemLength, 4);
+				Bi[3] = -2 / ElemLength + 6 * xi / Math.Pow(ElemLength, 2);
+
+				// Transform from global to local coordinates
+				LinearAlgebra.Vector<double> globalElemDispVector = LinearAlgebra.Vector<double>.Build.Dense(globalElemDisp.ToArray());
+				LinearAlgebra.Vector<double> localElemDisp = G.Multiply(globalElemDispVector);
+
+				// Extract displacements associated to beam dofs
+				LinearAlgebra.Vector<double> localElemDispBeam = LinearAlgebra.Vector<double>.Build.Dense(4);
+				localElemDispBeam[0] = localElemDisp[1];
+				localElemDispBeam[1] = localElemDisp[2];
+				localElemDispBeam[2] = localElemDisp[4];
+				localElemDispBeam[3] = localElemDisp[5];
+
+				// Particular solution for uniformly distributed load
+				double Mpi = TranverseLoad * (xi * xi / 2 - ElemLength * xi / 2 + ElemLength * ElemLength / 12);
+
+				M.Add(StiffnessModulus * MomentArea * (Bi.DotProduct(localElemDispBeam) + Mpi));
+			}
 		}
 
 	}
--- FrameDeformation/FrameDeformationComponent.cs
+++ FrameDeformation/FrameDeformationComponent.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using LinearAlgebra = MathNet.Numerics.LinearAlgebra;
+using System.Linq;
 
 using Grasshopper.Kernel;
 using Rhino.Geometry;
@@ -51,14 +53,211 @@ namespace FrameDeformation
 			DA.GetData("Scale Factor", ref scaleFactor);
 
 			// The length of A and E must be the same as lines
-			if ((A.Count != E.Count) | (E.Count != lines.Count))
+			if ((A.Count != E.Count) | (E.Count != lines.Count) | (I.Count != lines.Count))
 			{
-				throw new ArgumentException("Length of A and E must equal length of Line");
+				throw new ArgumentException("Length of A, I and E must equal length of Line");
 			}
 
 			// Create one list to store the nodes and one list to store the bars
-			List<Node> trussNodes = new List<Node>();
-			List<Bar> trussBars = new List<Bar>();
+			List<Node> frameNodes = new List<Node>();
+			List<Beam> frameBeams = new List<Beam>();
+
+
+			// Topology matrix to keep track of element dofs
+			List<List<int>> eDof = new List<List<int>>();
+
+
+			// Loop trough each line and create nodes at end points 
+			for (int i = 0; i < lines.Count; i++)
+			{
+
+				Node node1 = new Node(lines[i].From);
+				Node node2 = new Node(lines[i].To);
+
+				// To keep track if the node is unique
+				bool unique1 = true;
+				bool unique2 = true;
+
+
+				// Check if node is unique, if so give it an ID and degress of freedom
+				foreach (Node existingNode in frameNodes)
+				{
+
+					// If not unique use an already identified node
+					if (node1 == existingNode)
+					{
+						node1 = existingNode;
+						unique1 = false;
+					}
+
+					if (node2 == existingNode)
+					{
+						node2 = existingNode;
+						unique2 = false;
+					}
+				}
+
+				// If unique give it an ID
+				if (unique1)
+				{
+					int id_node_1 = frameNodes.Count;
+					node1.ID = id_node_1;
+					node1.Dofs = System.Linq.Enumerable.Range(id_node_1 * 3, 3).ToList();
+
+					// Check if any boundary node or load node exist at current node
+					foreach (ContstraintNode rNode in rNodes)
+					{
+						if (rNode == node1)
+						{
+							// Add restraint data
+							node1.ConstraintX = rNode.ConstraintX;
+							node1.ConstraintY = rNode.ConstraintY;
+							node1.ConstraintR = rNode.ConstraintR;
+						}
+					}
+
+					foreach (LoadNode loadNode in loadNodes)
+					{
+						if (loadNode == node1)
+						{
+							// Add force data
+							node1.ForceX = loadNode.ForceX;
+							node1.ForceY = loadNode.ForceY;
+							node1.MomentR = loadNode.MomentR;
+						}
+					}
+
+					// Finally add the node
+					frameNodes.Add(node1);
+
+				}
+
+				if (unique2)
+				{
+					int id_node_2 = frameNodes.Count;
+					node2.ID = id_node_2;
+					node2.Dofs = System.Linq.Enumerable.Range(id_node_2 * 3, 3).ToList();
+
+					// Check if any boundary node or load node exist at current node
+					foreach (ContstraintNode rNode in rNodes)
+					{
+						if (rNode == node2)
+						{
+							// Add constraint data
+							node2.ConstraintX = rNode.ConstraintX;
+							node2.ConstraintY = rNode.ConstraintY;
+							node2.ConstraintR = rNode.ConstraintR;
+						}
+					}
+
+
+					foreach (LoadNode loadNode in loadNodes)
+					{
+						if (loadNode == node2)
+						{
+							// Add force data
+							node2.ForceX = loadNode.ForceX;
+							node2.ForceY = loadNode.ForceY;
+							node2.MomentR = loadNode.MomentR;
+						}
+					}
+
+					// Finally add the node
+					frameNodes.Add(node2);
+				}
+
+
+				// Create a beam object between the nodes
+				Beam beam = new Beam(node1, node2, A[i], E[i], I[i]);
+				frameBeams.Add(beam);
+
+				// Topology matrix
+				List<int> dofs1 = beam.Nodes[0].Dofs;
+				List<int> dofs2 = beam.Nodes[1].Dofs;
+
+				List<int> eDofRow = new List<int>();
+				eDofRow.AddRange(dofs1);
+				eDofRow.AddRange(dofs2);
+				eDof.Add(eDofRow);
+
+			}
+
+			int nDof = frameNodes.Count * 3;
+			int nElem = eDof.Count;
+
+			// Loop trough each node and construct a load vector and boundary vector
+			LinearAlgebra.Vector<double> forceVector = LinearAlgebra.Vector<double>.Build.Dense(nDof);
+			List<int> boundaryDofs = new List<int>();
+			List<double?> boundaryConstraints = new List<double?>();
+
+			for (int i = 0; i < frameNodes.Count; i++)
+			{
+
+				// Load vector
+				forceVector[i * 3] = frameNodes[i].ForceX;
+				forceVector[i * 3 + 1] = frameNodes[i].ForceY;
+				forceVector[i * 3 + 2] = frameNodes[i].MomentR;
+
+				// Boundary vector
+				for (int j = 0; j < frameNodes[i].Dofs.Count; j++)
+				{
+					if (j == 0 && frameNodes[i].ConstraintX != null)
+					{
+						boundaryDofs.Add(frameNodes[i].Dofs[j]);
+						boundaryConstraints.Add(frameNodes[i].ConstraintX);
+					}
+					else if (j == 1 && frameNodes[i].ConstraintY != null)
+					{
+						boundaryDofs.Add(frameNodes[i].Dofs[j]);
+						boundaryConstraints.Add(frameNodes[i].ConstraintY);
+					}
+
+					else if (j == 2 && frameNodes[i].ConstraintR != null)
+					{
+						boundaryDofs.Add(frameNodes[i].Dofs[j]);
+						boundaryConstraints.Add(frameNodes[i].ConstraintR);
+					}
+
+				}
+			}
+
+			// Loop trough each element, compute local stiffness matrix and assemble into global stiffness matrix
+			LinearAlgebra.Matrix<double> K = LinearAlgebra.Matrix<double>.Build.Dense(nDof, nDof);
+
+			for (int i = 0; i < frameBeams.Count; i++)
+			{
+				LinearAlgebra.Matrix<double> KElem = frameBeams[i].ComputeStiffnessMatrix();
+
+				// Assemble
+				for (int k = 0; k < 6; k++)
+				{
+					for (int l = 0; l < 6; l++)
+					{
+						K[eDof[i][k], eDof[i][l]] = K[eDof[i][k], eDof[i][l]] + KElem[k, l];
+					}
+				}
+			}
+
+			// Calculate the displacements
+			Solver solver = new Solver();
+			LinearAlgebra.Vector<double> displacements = solver.solveEquations(K, forceVector, boundaryDofs, boundaryConstraints.Cast<double>().ToList());
+
+			// Loop trough each beam and compute the shear forces and bending moments and save in branches for each element
+			// Save the displacement for each node and calculate the stress in each bar
+			List<double> elemStress = new List<double> { };
+			for (int i = 0; i < nElem; i++)
+			{
+				double disp1 = displacements[eDof[i][0]];
+				double disp2 = displacements[eDof[i][1]];
+				double disp3 = displacements[eDof[i][2]];
+				double disp4 = displacements[eDof[i][3]];
+				double disp5 = displacements[eDof[i][4]];
+				double disp6 = displacements[eDof[i][5]];
+
+				// Calculate element shear force
+				frameBeams[i].computeShearForce(new List<double> { disp1, disp2, disp3, disp4, disp5, disp6 });
+
+			}
 		}
 
 
@@ -66,17 +265,10 @@ namespace FrameDeformation
 		{
 			get
 			{
-				// You can add image files to your project resources and access them like this:
-				//return Resources.IconForThisComponent;
 				return null;
 			}
 		}
 
-		/// <summary>
-		/// Each component must have a unique Guid to identify it. 
-		/// It is vital this Guid doesn't change otherwise old ghx files 
-		/// that use the old ID will partially fail during loading.
-		/// </summary>
 		public override Guid ComponentGuid
 		{
 			get { return new Guid("2e9e9f71-7c70-4a07-a149-a15dd30fc1b8"); }
